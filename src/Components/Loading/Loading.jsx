88b41b48d97814f90d259eb61c0cef368d0a3973
김용민 <hjk70288@naver.com>
2022-04-04 17:47:23 +0900

    모바일 환경에서 새로고침 시 무한 로딩 오류 제거

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,26 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./loading.module.scss";
 
 const Loading = props => {
   const { isLoaded } = props;
+  const loadingRef = useRef();
   const [afterLoad, setAfterLoad] = useState(false);
+
+  // 로딩 컴포넌트의 display을 0.5초 뒤에 none으로 변경
+  const removeLoadingDisplay = () => {
+    setTimeout(() => {
+      loadingRef.current.style.display = "none";
+    }, 500);
+  };
 
   // 로딩이 완료된 후 0.5초 후 실행
   if (isLoaded) {
     setTimeout(() => {
       setAfterLoad(true);
+      removeLoadingDisplay();
     }, 500);
+  }
+
+  // 모바일 환경인 경우
+  const userInfo = navigator.userAgent;
+  if (userInfo.indexOf("iPhone") > -1 || userInfo.indexOf("Android") > -1) {
+    // 로딩이 3초 이상 안되는 경우 강제로 로딩 제거 (모바일에서의 무한 로딩 오류 제거)
+    if (!isLoaded) {
+      setTimeout(() => {
+        setAfterLoad(true);
+        removeLoadingDisplay();
+      }, 2000);
+    }
   }
 
   return (
     <div
-      className={
-        !isLoaded
-          ? styles["before-load"]
-          : afterLoad
-          ? styles["after-load"]
-          : ""
-      }
+      ref={loadingRef}
+      className={!afterLoad ? styles["before-load"] : styles["after-load"]}
     >
       <div className={styles["loading"]}>
         <svg className={styles["loading__circle"]}>
