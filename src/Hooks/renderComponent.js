c03d318f89e8a4036c5f97d44a147a4074816f7f
김용민 <hjk70288@naver.com>
2022-04-13 16:39:44 +0900

    상세 페이지 > 모바일 환경에서 첫 렌더링 버벅임 현상 디버깅4

--- src/Hooks/renderComponent.js
+++ src/Hooks/renderComponent.js
@@ -1,5 +1,14 @@
 /* 렌더링 애니메이션 효과 및 페이지 이동 */
 const renderComponent = (history, url) => {
+  // document.body.classList.add("render");
+  // setTimeout(() => {
+  //   window.scrollTo(0, 0);
+  //   history.push(url);
+  //   setTimeout(() => {
+  //     document.body.classList.remove("render");
+  //   }, 500);
+  // }, 500);
+
   document.body.classList.add("render");
   setTimeout(() => {
     window.scrollTo(0, 0);
@@ -7,7 +16,7 @@ const renderComponent = (history, url) => {
     setTimeout(() => {
       document.body.classList.remove("render");
     }, 500);
-  }, 500);
+  }, 400);
 };
 
 export default renderComponent;
