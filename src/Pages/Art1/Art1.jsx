46aa0c5748417e9fead0f693629751178dbff578
김용민 <hjk70288@naver.com>
2022-04-14 15:11:10 +0900

    상세 페이지 > 스크롤 진행률 표시 컴포넌트 추가

--- src/Pages/Art1/Art1.jsx
+++ src/Pages/Art1/Art1.jsx
@@ -1,25 +1,34 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./art.module.scss";
 import Header from "Components/Header/header/DetailHeader";
+import Progress from "Components/Header/progress/Progress";
 import calcAnimationValues from "Hooks/calcAnimationValues";
 import renderComponent from "Hooks/renderComponent";
 
 const Art1 = ({ history }) => {
-  const backgroundRef = useRef();
-  const nextButtonRef = useRef();
-  const [startRender, setStartRender] = useState(false);
-  const [isRender, setIsRender] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
+  const progressRef = useRef(); // 스크롤 진행률 Ref
+  const nextBackgroundRef = useRef(); // Next 영역의 배경 Ref
+  const nextButtonRef = useRef(); // Next 영역의 버튼(메시지) Ref
+  const [startRender, setStartRender] = useState(false); // 애니메이션 시작 여부
+  const [isRender, setIsRender] = useState(false); // 애니메이션 완료 여부
+  const [isMobile, setIsMobile] = useState(false); // 사용자의 기기가 모바일인지에 대한 여부
 
+  // 스크롤 시 이벤트 핸들링
   const handlePageScroll = () => {
-    const scrollRatio = window.pageYOffset / document.body.offsetHeight;
+    const scrollRatio = window.pageYOffset / document.body.offsetHeight; // 스크롤 비율
     const animationStartRatio =
       (document.body.offsetHeight - window.innerHeight * 2) /
-      document.body.offsetHeight;
+      document.body.offsetHeight; // Next 영역 Opacity 애니메이션 시작 시점
 
+    // 스크롤 진행률 표시
+    if (progressRef.current) {
+      progressRef.current.style.width = `${scrollRatio * 134}%`;
+    }
+
+    // Next 영역 Opacity 애니메이션
     if (scrollRatio > animationStartRatio) {
-      if (backgroundRef.current) {
-        backgroundRef.current.style.opacity = calcAnimationValues(
+      if (nextBackgroundRef.current) {
+        nextBackgroundRef.current.style.opacity = calcAnimationValues(
           [0.1, 0.6, { start: 0, end: 1 }],
           window.innerHeight,
           window.pageYOffset - document.body.offsetHeight * animationStartRatio
@@ -63,6 +72,7 @@ const Art1 = ({ history }) => {
       }`}
     >
       <Header history={history} />
+      <Progress ref={progressRef} />
       <section className={styles["intro"]}>
         <div
           className={styles["intro__background"]}
@@ -87,7 +97,10 @@ const Art1 = ({ history }) => {
       <section className={styles["info"]}>대충 작품 설명하는 내용</section>
       <section className={styles["transparent-area"]}></section>
       <section className={styles["next"]}>
-        <div ref={backgroundRef} className={styles["next__background"]}></div>
+        <div
+          ref={nextBackgroundRef}
+          className={styles["next__background"]}
+        ></div>
         <div
           ref={nextButtonRef}
           className={styles["next__button"]}
